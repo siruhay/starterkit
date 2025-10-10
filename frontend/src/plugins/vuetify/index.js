0f19850af80209d6693296c7e0bbcb6acb5848d8
sudarmono <30992530+monoland@users.noreply.github.com>
2025-10-10 15:47:48 +0700

    update

--- frontend/src/plugins/vuetify/index.js
+++ frontend/src/plugins/vuetify/index.js
@@ -5,25 +5,23 @@ import { createVuetify } from "vuetify";
 import { VLigatureIcon } from "vuetify/components";
 import { aliases } from "vuetify/iconsets/md";
 import { VDateInput } from "vuetify/labs/VDateInput";
-import { VNumberInput } from "vuetify/labs/VNumberInput";
 
 export default createVuetify({
-	components: {
-		VDateInput,
-		VNumberInput,
-	},
+    components: {
+        VDateInput,
+    },
 
-	icons: {
-		defaultSet: "md",
-		aliases,
-		sets: {
-			md: {
-				component: (props) =>
-					h(VLigatureIcon, {
-						...props,
-						class: "material-symbols-outlined",
-					}),
-			},
-		},
-	},
+    icons: {
+        defaultSet: "md",
+        aliases,
+        sets: {
+            md: {
+                component: (props) =>
+                    h(VLigatureIcon, {
+                        ...props,
+                        class: "material-symbols-outlined",
+                    }),
+            },
+        },
+    },
 });
